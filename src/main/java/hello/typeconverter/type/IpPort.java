a1429fa27a7a356125357230ebb5c19cedc744a8
bsh6463 <bsh9510@gmail.com>
2021-10-01 23:02:09 +0900

    211001 ConversionService

--- src/main/java/hello/typeconverter/type/IpPort.java
+++ src/main/java/hello/typeconverter/type/IpPort.java
@@ -1,9 +1,10 @@
 package hello.typeconverter.type;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
-//field값이 모두 같으면 같은애로 처리.
+@EqualsAndHashCode//field값이 모두 같으면 같은애로 처리.
 public class IpPort {
 
     private String ip;
